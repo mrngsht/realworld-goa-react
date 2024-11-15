0a2aeffc59ea4136c5232f572ddb5b07753e78e8
mrngsht <mrngsht@gmail.com>
2024-11-15 09:21:25 +0900

    add tables for article

--- server/myrdb/migrations/20241007050421_init.sql
+++ server/myrdb/migrations/20241007050421_init.sql
@@ -72,9 +72,127 @@ CREATE TABLE IF NOT EXISTS user_follow_mutation_ (
   CONSTRAINT fk_followed_user_id_ FOREIGN KEY (followed_user_id_) REFERENCES user_ (id_)
 );
 
+CREATE TABLE IF NOT EXISTS article_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  id_ UUID NOT NULL,
+  PRIMARY KEY (id_)
+);
+
+CREATE TABLE IF NOT EXISTS article_content_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  updated_at_ TIMESTAMPTZ NOT NULL,
+  article_id_ UUID NOT NULL,
+  title_ TEXT NOT NULL,
+  description_ TEXT NOT NULL,
+  body_ TEXT NOT NULL,
+  author_user_id_ UUID NOT NULL,
+  PRIMARY KEY (article_id_),
+  CONSTRAINT fk_article_id_ FOREIGN KEY (article_id_) REFERENCES article_ (id_),
+  CONSTRAINT fk_author_user_id_ FOREIGN KEY (author_user_id_) REFERENCES user_ (id_)
+);
+
+CREATE TABLE IF NOT EXISTS article_content_mutation_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  article_id_ UUID NOT NULL,
+  title_ TEXT NOT NULL,
+  description_ TEXT NOT NULL,
+  body_ TEXT NOT NULL,
+  author_user_id_ UUID NOT NULL,
+  CONSTRAINT fk_article_id_ FOREIGN KEY (article_id_) REFERENCES article_ (id_),
+  CONSTRAINT fk_author_user_id_ FOREIGN KEY (author_user_id_) REFERENCES user_ (id_)
+);
+
+CREATE TABLE IF NOT EXISTS article_deleted_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  article_id_ UUID NOT NULL,
+  PRIMARY KEY (article_id_),
+  CONSTRAINT fk_article_id_ FOREIGN KEY (article_id_) REFERENCES article_ (id_)
+);
+
+CREATE TABLE IF NOT EXISTS article_tag_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  article_id_ UUID NOT NULL,
+  tag_ TEXT NOT NULL,
+  PRIMARY KEY (article_id_, tag_),
+  CONSTRAINT fk_article_id_ FOREIGN KEY (article_id_) REFERENCES article_ (id_)
+);
+
+CREATE TABLE IF NOT EXISTS enum_article_tag_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  tag_ TEXT NOT NULL,
+  PRIMARY KEY (tag_)
+);
+
+CREATE TABLE IF NOT EXISTS article_comment_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  id_ UUID NOT NULL,
+  PRIMARY KEY (id_)
+);
+
+CREATE TABLE IF NOT EXISTS article_comment_content_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  article_comment_id_ UUID NOT NULL,
+  article_id_ UUID NOT NULL,
+  body_ TEXT NOT NULL,
+  user_id_ UUID NOT NULL,
+  PRIMARY KEY (article_comment_id_),
+  CONSTRAINT fk_article_comment_id_ FOREIGN KEY (article_comment_id_) REFERENCES article_comment_ (id_),
+  CONSTRAINT fk_article_id_ FOREIGN KEY (article_id_) REFERENCES article_ (id_),
+  CONSTRAINT fk_user_id_ FOREIGN KEY (user_id_) REFERENCES user_ (id_)
+);
+
+CREATE TABLE IF NOT EXISTS article_comment_content_mutation_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  article_comment_id_ UUID NOT NULL,
+  article_id_ UUID NOT NULL,
+  body_ TEXT NOT NULL,
+  user_id_ UUID NOT NULL,
+  CONSTRAINT fk_article_comment_id_ FOREIGN KEY (article_comment_id_) REFERENCES article_comment_ (id_),
+  CONSTRAINT fk_article_id_ FOREIGN KEY (article_id_) REFERENCES article_ (id_),
+  CONSTRAINT fk_user_id_ FOREIGN KEY (user_id_) REFERENCES user_ (id_)
+);
+
+CREATE TABLE IF NOT EXISTS article_comment_deleted_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  article_comment_id_ UUID NOT NULL,
+  PRIMARY KEY (article_comment_id_),
+  CONSTRAINT fk_article_comment_id_ FOREIGN KEY (article_comment_id_) REFERENCES article_comment_ (id_)
+);
+
+CREATE TABLE IF NOT EXISTS article_favorite_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  article_id_ UUID NOT NULL,
+  user_id_ UUID NOT NULL,
+  PRIMARY KEY (article_id_, user_id_),
+  CONSTRAINT fk_article_id_ FOREIGN KEY (article_id_) REFERENCES article_ (id_),
+  CONSTRAINT fk_user_id_ FOREIGN KEY (user_id_) REFERENCES user_ (id_)
+);
+
+CREATE TYPE article_favorite_mutation_type_ AS ENUM ('favorite', 'unfavorite');
+
+CREATE TABLE IF NOT EXISTS article_favorite_mutation_ (
+  created_at_ TIMESTAMPTZ NOT NULL,
+  article_id_ UUID NOT NULL,
+  user_id_ UUID NOT NULL,
+  type_ article_favorite_mutation_type_ NOT NULL,
+  CONSTRAINT fk_article_id_ FOREIGN KEY (article_id_) REFERENCES article_ (id_),
+  CONSTRAINT fk_user_id_ FOREIGN KEY (user_id_) REFERENCES user_ (id_)
+);
 
 -- +goose Down
 -- +goose StatementBegin
+DROP TABLE IF EXISTS article_favorite_mutation_;
+DROP TABLE IF EXISTS article_favorite_;
+DROP TABLE IF EXISTS article_comment_deleted_;
+DROP TABLE IF EXISTS article_comment_content_mutation_;
+DROP TABLE IF EXISTS article_comment_content_;
+DROP TABLE IF EXISTS article_comment_;
+DROP TABLE IF EXISTS enum_article_tag_;
+DROP TABLE IF EXISTS article_tag_;
+DROP TABLE IF EXISTS article_deleted_;
+DROP TABLE IF EXISTS article_content_mutation_;
+DROP TABLE IF EXISTS article_content_;
+DROP TABLE IF EXISTS article_;
 DROP TABLE IF EXISTS user_follow_mutation_;
 DROP TABLE IF EXISTS user_follow_;
 DROP TABLE IF EXISTS user_follow_;
@@ -85,5 +203,6 @@ DROP TABLE IF EXISTS user_profile_mutation_;
 DROP TABLE IF EXISTS user_profile_;
 DROP TABLE IF EXISTS user_;
 
-DROP TYPE user_follow_mutation_type_;
+DROP TYPE IF EXISTS article_favorite_mutation_type_;
+DROP TYPE IF EXISTS user_follow_mutation_type_;
 -- +goose StatementEnd
--- server/myrdb/schema.sql
+++ server/myrdb/schema.sql
@@ -1,6 +1,77 @@
+CREATE TYPE "article_favorite_mutation_type_" AS ENUM (
+    'favorite',
+    'unfavorite'
+);
 CREATE TYPE "user_follow_mutation_type_" AS ENUM (
     'follow',
     'unfollow'
+);
+CREATE TABLE "article_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "id_" "uuid" NOT NULL
+);
+CREATE TABLE "article_comment_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "id_" "uuid" NOT NULL
+);
+CREATE TABLE "article_comment_content_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "article_comment_id_" "uuid" NOT NULL,
+    "article_id_" "uuid" NOT NULL,
+    "body_" "text" NOT NULL,
+    "user_id_" "uuid" NOT NULL
+);
+CREATE TABLE "article_comment_content_mutation_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "article_comment_id_" "uuid" NOT NULL,
+    "article_id_" "uuid" NOT NULL,
+    "body_" "text" NOT NULL,
+    "user_id_" "uuid" NOT NULL
+);
+CREATE TABLE "article_comment_deleted_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "article_comment_id_" "uuid" NOT NULL
+);
+CREATE TABLE "article_content_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "updated_at_" timestamp with time zone NOT NULL,
+    "article_id_" "uuid" NOT NULL,
+    "title_" "text" NOT NULL,
+    "description_" "text" NOT NULL,
+    "body_" "text" NOT NULL,
+    "author_user_id_" "uuid" NOT NULL
+);
+CREATE TABLE "article_content_mutation_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "article_id_" "uuid" NOT NULL,
+    "title_" "text" NOT NULL,
+    "description_" "text" NOT NULL,
+    "body_" "text" NOT NULL,
+    "author_user_id_" "uuid" NOT NULL
+);
+CREATE TABLE "article_deleted_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "article_id_" "uuid" NOT NULL
+);
+CREATE TABLE "article_favorite_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "article_id_" "uuid" NOT NULL,
+    "user_id_" "uuid" NOT NULL
+);
+CREATE TABLE "article_favorite_mutation_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "article_id_" "uuid" NOT NULL,
+    "user_id_" "uuid" NOT NULL,
+    "type_" "article_favorite_mutation_type_" NOT NULL
+);
+CREATE TABLE "article_tag_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "article_id_" "uuid" NOT NULL,
+    "tag_" "text" NOT NULL
+);
+CREATE TABLE "enum_article_tag_" (
+    "created_at_" timestamp with time zone NOT NULL,
+    "tag_" "text" NOT NULL
 );
 CREATE TABLE "user_" (
     "created_at_" timestamp with time zone NOT NULL,
@@ -49,6 +120,24 @@ CREATE TABLE "user_profile_mutation_" (
     "bio_" "text" NOT NULL,
     "image_url_" "text" NOT NULL
 );
+ALTER TABLE ONLY "article_"
+    ADD CONSTRAINT "article__pkey" PRIMARY KEY ("id_");
+ALTER TABLE ONLY "article_comment_"
+    ADD CONSTRAINT "article_comment__pkey" PRIMARY KEY ("id_");
+ALTER TABLE ONLY "article_comment_content_"
+    ADD CONSTRAINT "article_comment_content__pkey" PRIMARY KEY ("article_comment_id_");
+ALTER TABLE ONLY "article_comment_deleted_"
+    ADD CONSTRAINT "article_comment_deleted__pkey" PRIMARY KEY ("article_comment_id_");
+ALTER TABLE ONLY "article_content_"
+    ADD CONSTRAINT "article_content__pkey" PRIMARY KEY ("article_id_");
+ALTER TABLE ONLY "article_deleted_"
+    ADD CONSTRAINT "article_deleted__pkey" PRIMARY KEY ("article_id_");
+ALTER TABLE ONLY "article_favorite_"
+    ADD CONSTRAINT "article_favorite__pkey" PRIMARY KEY ("article_id_", "user_id_");
+ALTER TABLE ONLY "article_tag_"
+    ADD CONSTRAINT "article_tag__pkey" PRIMARY KEY ("article_id_", "tag_");
+ALTER TABLE ONLY "enum_article_tag_"
+    ADD CONSTRAINT "enum_article_tag__pkey" PRIMARY KEY ("tag_");
 ALTER TABLE ONLY "user_"
     ADD CONSTRAINT "user__pkey" PRIMARY KEY ("id_");
 ALTER TABLE ONLY "user_auth_password_"
@@ -63,6 +152,32 @@ ALTER TABLE ONLY "user_profile_"
     ADD CONSTRAINT "user_profile__pkey" PRIMARY KEY ("user_id_");
 ALTER TABLE ONLY "user_profile_"
     ADD CONSTRAINT "user_profile__username__key" UNIQUE ("username_");
+ALTER TABLE ONLY "article_comment_content_"
+    ADD CONSTRAINT "fk_article_comment_id_" FOREIGN KEY ("article_comment_id_") REFERENCES "article_comment_"("id_");
+ALTER TABLE ONLY "article_comment_content_mutation_"
+    ADD CONSTRAINT "fk_article_comment_id_" FOREIGN KEY ("article_comment_id_") REFERENCES "article_comment_"("id_");
+ALTER TABLE ONLY "article_comment_deleted_"
+    ADD CONSTRAINT "fk_article_comment_id_" FOREIGN KEY ("article_comment_id_") REFERENCES "article_comment_"("id_");
+ALTER TABLE ONLY "article_content_"
+    ADD CONSTRAINT "fk_article_id_" FOREIGN KEY ("article_id_") REFERENCES "article_"("id_");
+ALTER TABLE ONLY "article_content_mutation_"
+    ADD CONSTRAINT "fk_article_id_" FOREIGN KEY ("article_id_") REFERENCES "article_"("id_");
+ALTER TABLE ONLY "article_deleted_"
+    ADD CONSTRAINT "fk_article_id_" FOREIGN KEY ("article_id_") REFERENCES "article_"("id_");
+ALTER TABLE ONLY "article_tag_"
+    ADD CONSTRAINT "fk_article_id_" FOREIGN KEY ("article_id_") REFERENCES "article_"("id_");
+ALTER TABLE ONLY "article_comment_content_"
+    ADD CONSTRAINT "fk_article_id_" FOREIGN KEY ("article_id_") REFERENCES "article_"("id_");
+ALTER TABLE ONLY "article_comment_content_mutation_"
+    ADD CONSTRAINT "fk_article_id_" FOREIGN KEY ("article_id_") REFERENCES "article_"("id_");
+ALTER TABLE ONLY "article_favorite_"
+    ADD CONSTRAINT "fk_article_id_" FOREIGN KEY ("article_id_") REFERENCES "article_"("id_");
+ALTER TABLE ONLY "article_favorite_mutation_"
+    ADD CONSTRAINT "fk_article_id_" FOREIGN KEY ("article_id_") REFERENCES "article_"("id_");
+ALTER TABLE ONLY "article_content_"
+    ADD CONSTRAINT "fk_author_user_id_" FOREIGN KEY ("author_user_id_") REFERENCES "user_"("id_");
+ALTER TABLE ONLY "article_content_mutation_"
+    ADD CONSTRAINT "fk_author_user_id_" FOREIGN KEY ("author_user_id_") REFERENCES "user_"("id_");
 ALTER TABLE ONLY "user_follow_"
     ADD CONSTRAINT "fk_followed_user_id_" FOREIGN KEY ("followed_user_id_") REFERENCES "user_"("id_");
 ALTER TABLE ONLY "user_follow_mutation_"
@@ -80,4 +195,12 @@ ALTER TABLE ONLY "user_auth_password_"
 ALTER TABLE ONLY "user_follow_"
     ADD CONSTRAINT "fk_user_id_" FOREIGN KEY ("user_id_") REFERENCES "user_"("id_");
 ALTER TABLE ONLY "user_follow_mutation_"
+    ADD CONSTRAINT "fk_user_id_" FOREIGN KEY ("user_id_") REFERENCES "user_"("id_");
+ALTER TABLE ONLY "article_comment_content_"
+    ADD CONSTRAINT "fk_user_id_" FOREIGN KEY ("user_id_") REFERENCES "user_"("id_");
+ALTER TABLE ONLY "article_comment_content_mutation_"
+    ADD CONSTRAINT "fk_user_id_" FOREIGN KEY ("user_id_") REFERENCES "user_"("id_");
+ALTER TABLE ONLY "article_favorite_"
+    ADD CONSTRAINT "fk_user_id_" FOREIGN KEY ("user_id_") REFERENCES "user_"("id_");
+ALTER TABLE ONLY "article_favorite_mutation_"
     ADD CONSTRAINT "fk_user_id_" FOREIGN KEY ("user_id_") REFERENCES "user_"("id_");
